821ce68592d0b3c303f4e9503ba2988a3461211d
PETEletricaUFC <pet@dee.ufc.br>
2025-01-17 09:16:55 -0300

    Update Questão 03.asm

--- Prática\AP1/PL1_2018_A/Questão 03.asm
+++ Prática\AP1/PL1_2018_A/Questão 03.asm
@@ -1,87 +1,81 @@
-; Programa para PIC16F628A - USART: Receber caracteres de '0' a '8' e retornar '1' a '9'
-; Clock: 10 MHz
+#INCLUDE <P16F628A.INC>
 
-    #include <P16F628A.inc>       ; Arquivo de definição do microcontrolador
+#DEFINE LED1   PORTB,6        ; Define LED1 no pino RB6
+#DEFINE LED2   PORTB,7        ; Define LED2 no pino RB7
 
-    __config _INTRC_OSC_NOCLKOUT & _WDT_OFF & _PWRTE_ON & _MCLRE_OFF
+at_x        EQU   0x70        ; Registrador para contador de atraso externo
+at_tmp1     EQU   0x71        ; Registrador para contador de atraso interno
+at_tmp2     EQU   0x72        ; Registrador para contador mais interno
 
-;------------------------------------------
-; Definição de constantes
-;------------------------------------------
-
-
-;------------------------------------------
-; Variáveis
-;------------------------------------------
-    cblock  0x20                   ; Início da RAM para variáveis
-        received_char             ; Caractere recebido via USART
-    endc
+temp        EQU   0x74        ; Registrador temporário usado para armazenar dados recebidos
 
 ;------------------------------------------
-; Inicialização
+; Vetores de Reset e Interrupção
 ;------------------------------------------
-    org 0x0000                    ; Início do programa
-    goto    Start                    ; Pula para o início do programa
+ORG 0                  ; Vetor de reset
+GOTO INI               ; Salta para o início do programa
+
+ORG 4                  ; Vetor de interrupção
+RETFIE                 ; Retorna da interrupção
+
+;------------------------------------------
+; Inicialização do Programa
+;------------------------------------------
+INI:
+
+    ; Configuração de TRISA e TRISB
+    BANKSEL TRISA      ; Seleciona o banco para acessar TRISA
+    movlw 0xff         ; Configura todos os pinos de PORTA como saída
+    movwf TRISA        ; Escreve no registrador TRISA
+    movlw b'00000010'         ; Configura os pinos RB0-RB5 como entrada e RB6-RB7 como saída
+    movwf TRISB        ; Escreve no registrador TRISB
+
+    ; Configuração do módulo UART
+    BANKSEL SPBRG      ; Seleciona o banco para acessar SPBRG
+    movlw d'31'          ; Configura baud rate para 19200 (exemplo)
+    movwf SPBRG        ; Escreve no registrador SPBRG
     
-    org 0x0004
-    goto    Receba
-    retfie
-
-Start:
-    ; Configuração USART
-    banksel TXSTA
-    movlw   b'00100100'           ; Configurações: BRGH = 1, modo assíncrono
-    movwf   TXSTA                 ; Habilita transmissão
-
-    banksel RCSTA
-    movlw   b'10010000'           ; Habilita recepção e módulo serial
-    movwf   RCSTA
-
-    banksel SPBRG
-    movlw   d'25'                 ; Configura baud rate: 9600 bps com Fosc = 10 MHz
-    movwf   SPBRG
-
-    banksel PIR1
-    bcf     PIR1, RCIF            ; Limpa flag de recepção
+    BANKSEL TXSTA      ; Seleciona o banco para acessar TXSTA
+    bsf  TXSTA,TXEN    ; Habilita o transmissor UART
+    bsf  TXSTA,BRGH    ; Configura alta velocidade UART
     
-    banksel INTCON
-    bsf	INTCON, GIE
-    bsf INTCON,	T0IE
-    
-    BANKSEL TRISB
-    MOVLW 0x00
-    MOVWF TRISB
+    BANKSEL RCSTA      ; Seleciona o banco para acessar RCSTA
+    bsf  RCSTA,SPEN    ; Habilita o módulo UART
+    bsf  RCSTA,CREN    ; Habilita recepção contínua
+
+    ; Configuração do registrador INTCON
+    BANKSEL INTCON     ; Seleciona o banco para acessar INTCON
+    movlw 0x00         ; Desabilita todas as interrupções
+    movwf INTCON       ; Escreve no registrador INTCON
 
 ;------------------------------------------
-; Loop principal
+; Loop Principal
 ;------------------------------------------
-MainLoop:
+MAIN:
+    ; Verifica se há dado recebido pela UART
+    BANKSEL PIR1       ; Seleciona o banco para acessar PIR1
+    BTFSC PIR1,RCIF    ; Testa se o bit RCIF (flag de recepção UART) está setado
+    ;BANKSEL   TXREG
+    ;CLRF    TXREG
+    CALL  rec_e_trata  ; Se RCIF = 1, chama a sub-rotina para tratar o dado recebido
 
-    goto    MainLoop              ; Volta para o loop principal
+    GOTO MAIN          ; Retorna ao início do loop principal
 
 ;------------------------------------------
-; Sub-rotinas
+; Sub-rotina para Recepção e Tratamento do Dado
 ;------------------------------------------
-Receba:
-    banksel PIR1
-    bcf	PIR1, RCIF
+rec_e_trata:
     
-    banksel RCREG
-    movf    RCREG, W              ; Lê o dado recebido
-    movwf   received_char         ; Armazena o caractere recebido
+ 
+    BANKSEL RCREG      ; Seleciona o banco para acessar RCREG
+    INCF RCREG,	W        ; Move o dado recebido de RCREG para WREG
     
-    banksel received_char
-    movf    received_char, W      ; Move o caractere recebido para W
-    addlw   d'1'                  ; Incrementa o valor ASCII em 1
-    
-    banksel PIR1
-    bsf     PIR1, TXIF            ; Sinaliza que o buffer está pronto para transmissão
-    btfss   PIR1, TXIF  ; Verifica se o buffer de transmissão está vazio
-    goto    $-1          ; Aguarda até estar pronto
+    BANKSEL   TXREG
+    MOVWF	TXREG
+    CLRW
 
-    banksel TXREG
-    movwf   TXREG                 ; Envia o caractere incrementado
 
-    return
+    return             ; Retorna para o chamador
 
-    end
+
+END                     ; Indica o fim do programa
