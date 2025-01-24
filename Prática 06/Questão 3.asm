f78ed9b1aa551be05b86edb27b84eea3e2f56257
Geová Machado <133471408+geova16@users.noreply.github.com>
2025-01-24 15:02:08 -0300

    Update Questão 3.asm

--- Prática 06/Questão 3.asm
+++ Prática 06/Questão 3.asm
@@ -1,8 +1,6 @@
 org 100h
-; Define a origem do código em 100h, padrão para programas .COM no DOS.
 
-key:
-; Rótulo que marca o início do loop de espera por entrada.
+loop_entrada:
 
 mov AH, 6h
 ; Configura o registrador AH com o valor 6.
@@ -16,14 +14,18 @@ mov DL, 0FFh
 int 21h
 ; Chama a interrupção 21h para executar a função configurada.
 
-jz key
+jz loop_entrada
 ; Verifica o flag ZF (Zero Flag).
-; Se nenhuma tecla foi pressionada (ZF = 1), o programa volta ao início (`key`).
+; Se nenhuma tecla foi pressionada (ZF = 1), o programa volta ao rotulo loop_entrada.
 ; Isso cria um loop contínuo até que uma tecla seja pressionada.
+ 
+ 
+ 
+ 
 
 mov AH, AL
 ; Move o valor de AL (caractere da tecla pressionada) para AH.
-; Este valor será usado mais tarde nas comparações.
+
 
 xor AL, 'y'
 ; Realiza uma operação XOR entre o caractere pressionado (AL) e o caractere `'y'` (121 ASCII).
@@ -32,28 +34,27 @@ xor AL, 'y'
 jne letran
 ; Se o resultado da XOR não for zero (tecla pressionada não foi `'y'`), salta para o rótulo `letran`.
 
+
+
+
 mov AH, 2
 ; Configura o registrador AH com o valor 2.
 ; A interrupção 21h com AH = 2 é usada para exibir caracteres no console.
 
 mov DL, 'S'
-; Define o valor de DL como `'S'` (83 em ASCII).
-
 int 21h
-; Exibe o caractere `'S'` no console.
 
 mov DL, 'I'
 int 21h
-; Exibe o caractere `'I'` no console.
 
 mov DL, 'M'
 int 21h
-; Exibe o caractere `'M'` no console.
 
-ret
-; Retorna ao sistema operacional, finalizando o programa.
 
-;---
+jmp jump1
+ 
+ 
+ 
 
 letran:
 ; Rótulo para processar se a tecla pressionada não foi `'y'`.
@@ -70,24 +71,19 @@ mov AH, 2
 
 mov DL, 'N'
 int 21h
-; Exibe `'N'` no console.
 
 mov DL, 'A'
 int 21h
-; Exibe `'A'` no console.
 
 mov DL, 'O'
-int 21h
-; Exibe `'O'` no console.
+int 21h  
 
-mov AH, 0
-; Configura o registrador AH para esperar entrada de tecla.
 
-int 16h
-; Aguarda que o usuário pressione qualquer tecla antes de continuar.
+jump1:
+
+jmp loop_entrada
 
 letray:
-; Rótulo final.
+
 
 ret
-; Retorna ao sistema operacional, finalizando o programa.
