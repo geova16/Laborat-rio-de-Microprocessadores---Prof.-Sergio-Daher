876195a03e4f1508fa04084e0cbb19f7675d717a
Geová Machado <133471408+geova16@users.noreply.github.com>
2025-01-24 15:18:07 -0300

    Update Questão 4.asm

--- Prática 06/Questão 4.asm
+++ Prática 06/Questão 4.asm
@@ -1,5 +1,7 @@
 org 100h
-
+ 
+ini:
+ 
 ; Lê o primeiro número
 mov AH, 1          ; Configura a função de leitura de caractere (int 21h, AH=1)
 int 21h            ; Lê o caractere digitado do teclado
@@ -17,12 +19,27 @@ add BL, BH         ; Soma os valores armazenados em BL e BH
 mov AL, BL         ; Move o resultado da soma para AL
 
 ; Converte o resultado para ASCII
-add AL, 48         ; Converte o número decimal em caractere ASCII
+add AL, 48         ; Converte o número decimal em caractere ASCII 
+
+mov CL, AL
 
 ; Exibe o resultado
-mov DL, AL         ; Move o caractere resultante para DL
 mov AH, 2          ; Configura a função de exibição de caractere (int 21h, AH=2)
+
+mov DL, '='        
 int 21h            ; Exibe o caractere na tela
+mov DL, ' '        
+int 21h            ; Exibe o caractere na tela
+
+mov DL, CL
+int 21h
+
+
+mov DL, ' '
+mov AH, 2  
+int 21h
+
+jmp ini
 
 ; Fim do programa
 ret
