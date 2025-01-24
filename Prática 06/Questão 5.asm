2b606248303dc30661359961a0da8607be642c14
Geová Machado <133471408+geova16@users.noreply.github.com>
2025-01-24 15:59:11 -0300

    Update Questão 5.asm

--- Prática 06/Questão 5.asm
+++ Prática 06/Questão 5.asm
@@ -1,20 +1,22 @@
-ORG 100h         ; Início do programa COM
+ORG 100h        
           
 jmp INICIO          
           
-; Declaração de variáveis
-BUF DB 10       ; Primeiro byte: tamanho máximo do buffer (255 caracteres)
+
+BUF DB 10       ; Primeiro byte: tamanho máximo do buffer (10 caracteres)
     DB 0         ; Segundo byte: número de caracteres lidos (inicialmente 0)
-    DB 10 DUP (?) ; Espaço para armazenar o texto digitado (até 255 caracteres)
+    DB 10 DUP (?) ; Espaço para armazenar o texto digitado (até 10 caracteres)
+
 MSG DB 0Dh, 0Ah, 'No de caracteres = ', '$' ; Mensagem base para exibição
 
-; Início do código
+
 INICIO:
+
 LOOP_ENTRADA:
     ; Entrada da frase - Aguarda até o usuário digitar algo
     MOV DX, OFFSET BUF   ; Aponta o endereço do buffer para DX
     MOV AH, 0Ah          ; Função de entrada de buffer (DOS: INT 21h, AH=0Ah)
-    INT 21h              ; Chamada de interrupção (aguarda a entrada do usuário)
+    INT 21h              
 
     ; Verificar se algo foi digitado
     MOV AL, [BUF + 1]    ; Carrega o número de caracteres lidos no registrador AL
@@ -24,16 +26,24 @@ LOOP_ENTRADA:
     ; Exibir a mensagem de "No de caracteres = "
     MOV DX, OFFSET MSG     ; Aponta para a mensagem de base
     MOV AH, 09h            ; Função para exibição de string (DOS: INT 21h, AH=09h)
-    INT 21h                ; Chamada de interrupção para exibir a mensagem
+    INT 21h                
     
     ; Exibir o número de caracteres lidos
     MOV AL, [BUF + 1]      ; Carrega o número de caracteres lidos no registro AL
     ADD AL, 48             ; Converte o valor para o equivalente ASCII
     MOV DL, AL             ; Move o valor ASCII para DL
     MOV AH, 02h            ; Função para exibição de caractere (DOS: INT 21h, AH=02h)
-    INT 21h                ; Chamada de interrupção para exibir o caractere
+    INT 21h                     
     
-    ; Loop para continuar lendo frases
-    JMP INICIO             ; Salta para o início para nova entrada
+    ; Codigo para quebra de linha
+    MOV AH, 2
+    MOV DL, 10
+    INT 21h
+    
+    MOV DL, 13
+    INT 21h
+    
+    
+    JMP INICIO             
 
-END                        ; Fim do programa
+END                        
