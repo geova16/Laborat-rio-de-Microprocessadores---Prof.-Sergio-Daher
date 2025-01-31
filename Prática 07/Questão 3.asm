758af26207ae046dc6e07f21a93082db7d5d7d30
Geová Machado <133471408+geova16@users.noreply.github.com>
2025-01-30 23:20:17 -0300

    Create Questão 3.asm

---
+++ Prática 07/Questão 3.asm
@@ -0,0 +1,194 @@
+org 100h
+
+jmp ini             
+
+    label1 db "x = $"  
+    
+    label2 db "2*x = $"  
+    
+    quebra_lin db 0x0d,0x0a,'$'
+
+ini:
+    lea     dx, label1  
+    
+    mov     ah, 9
+    
+    int     21h              ; Imprime label1
+         
+         
+    mov     ah, 1
+    
+    int     21h              ; Le um caractere (trava o codigo)
+         
+    sub     al, 48           ; De ASCII para decimal 
+    
+    mov     ah, 0 
+    
+    mov     bx, 10
+    
+    mul     bx
+    
+    mul     bx
+    
+    mul     bx               ; Multiplica o numero lido por 1000 p jogar ele 3 casas pra esquerda
+          
+    push    ax               ; Guarda na pilha
+    
+    
+    mov     ah, 1 
+    
+    int     21h              ; Le um caractere (trava o codigo)  
+             
+    sub     al, 48           ; De ASCII para decimal   
+    
+    mov     ah, 0 
+    
+    mov     bx, 100
+    
+    mul     bx               ; Multiplica o numero lido por 100 p jogar ele 2 casas pra esquerda
+    
+    mov     cx, 0
+    
+    mov     cx, ax           ; Guarda em CX
+                             
+    pop     ax               ; Puxa o numero milhar da pilha
+    
+    add     ax, cx           ; Soma o milhar com a centena
+    
+    push    ax               ; Guarda novamente na pilha
+         
+        
+         
+    mov     ah, 1
+    
+    int     21h              ; Le um caractere (trava o codigo)
+         
+    sub     al, 48           ; De ASCII para decimal  
+    
+    mov     ah, 0
+    
+    mov     bx, 10           ; Multiplica o numero lido por 10 p jogar ele 1 casa pra esquerda
+    
+    mul     bx
+    
+    mov     cx, 0
+    
+    mov     cx, ax           ; Guarda em CX 
+    
+    pop     ax               ; Puxa o numero todo da pilha
+    
+    add     ax, cx           ; Soma o numero com a dezena
+    
+    push    ax               ; Guarda novamente na pilha   
+          
+    
+    
+    mov ah, 1 
+    
+    int 21h              ; Le um caractere (trava o codigo)  
+             
+    sub al, 48           ; De ASCII para decimal
+    
+    mov     ah, 0
+    
+    mov     cx, 0
+    
+    mov     cx, ax           ; Guarda em CX 
+    
+    pop     ax               ; Puxa o numero todo da pilha
+    
+    add     ax, cx           ; Soma o numero com a dezena
+    
+    push    ax               ; Guarda novamente na pilha 
+    
+     
+       
+    
+    lea dx,quebra_lin 
+    
+    mov ah,9      
+    
+    int 21h              ; Imprime quebra_lin
+    
+    
+    lea dx,label2 
+    
+    mov ah,9  
+                        
+    int 21h             ; Imprime label2
+    
+    
+    pop ax              ; Resgata a soma da pilha
+    
+    mov dx,2
+    
+    mul dx              ; Multiplica a soma por 2
+    
+    
+    call imprime_4d     ; Imprime a soma*2
+    
+    .EXIT       
+    
+    
+imprime_4d:
+    
+    ; Imprime a variavel anteriormente armazenada em AX (4 digitos)    
+    
+    mov DL, 100
+    
+    div DL                       ; Divide AX por 100 - separar o (milhar e centena) da (dezena e unidade)
+    
+    push AX                      ; Guarda os valores na pilha
+    
+    
+    
+    mov CH, AL                   ; Milhar e centena para CH
+    
+    mov AX, 0
+    
+    mov AL, CH                   ; Milhar e centena para AL    
+    
+    call imprime_2d              ; Imprime os dois digitos de AL
+    
+    
+    pop AX                       ; Recupera os valores guardados na pilha
+    
+    mov CL, AH                   ; Dezena e unidade para CL
+    
+    mov AX, 0
+    
+    mov AL, CL                   ; Dezena e unidade para AL 
+    
+    call imprime_2d              ; Imprime os dois digitos de AL
+    
+    ret
+    
+
+imprime_2d: 
+
+    ; Imprime o que ta em AL
+
+    mov ah,0 
+    
+    mov dl,10  
+    
+    div dl   
+    
+    mov dl,al 
+    
+    mov cl,ah
+    
+    mov ah,2 
+    
+    add dl,48 
+    
+    int 21h  
+    
+    
+    mov dl,cl 
+    
+    add dl,48 
+    
+    int 21h   
+    
+    ret  
