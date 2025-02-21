ec8ca92b0322be1b7c907cc62c8a5b83e797333d
Geová Machado <133471408+geova16@users.noreply.github.com>
2025-02-21 11:27:08 -0300

    Create le_2_digitos.asm

---
+++ Codigos das provas/Subrotinas/le_2_digitos.asm
@@ -0,0 +1,50 @@
+org 100h
+
+jmp start
+
+num db 0,0
+
+
+le_5_digitos: 
+
+    ; le 5 digitos no teclado e armazena am ax
+    
+    push bx
+    push cx
+    push dx
+    mov ax, 0    
+
+    start:
+        lea si, num
+        mov cx, 2
+    le_looping: 
+        mov ah, 1
+        int 21h 
+        sub al, 48
+        mov [si], al 
+        inc si
+    loop le_looping
+    
+    multiplica:  
+        dec si
+        mov dx, 0
+        mov bx, 1
+        mov di, 0
+        mov cx, 2 
+    rotina:
+        mov al, [si]
+        mov ah, 0
+        mul bx
+        add di, ax
+        mov ax, 10
+        mul bx
+        mov bx, ax
+        dec si
+        loop rotina   
+        mov ax, di 
+        
+        pop dx
+        pop cx
+        pop bx
+        
+        ret
